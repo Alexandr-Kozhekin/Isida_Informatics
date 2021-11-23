acf557ea0502743d2221079d5d1a0505127c649d
AlexandrKozhekin <alexandrkozhekin@gmail.com>
2021-11-23 23:13:47 +0300

    add web controller

---
+++ src/main/java/isida_inform/web_ui/web_controllers/InfoAboutLinkController.java
@@ -0,0 +1,46 @@
+package isida_inform.web_ui.web_controllers;
+
+
+import isida_inform.core.requests.LinkAnalysisRequests;
+import isida_inform.core.responses.LinkAnalysisResponse;
+import isida_inform.core.services.LinkAnalysisServices;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class InfoAboutLinkController {
+
+    @Autowired private LinkAnalysisServices services;
+
+    @GetMapping(value = "/")
+    public String showLinkAnalysisPage(ModelMap modelMap) {
+
+        modelMap.addAttribute("request", new LinkAnalysisRequests());
+
+        return "linkAnalysis";
+    }
+
+    @PostMapping("/")
+    public String processLinkAnalysisRequest(@ModelAttribute(value = "request") LinkAnalysisRequests requests,
+                                             ModelMap modelMap) {
+
+        LinkAnalysisResponse response = services.execute(requests);
+
+        if (response.hasErrors()) {
+
+            modelMap.addAttribute("errors", response.getErrors());
+            return "linkAnalysis";
+
+        } else {
+
+            modelMap.addAttribute("LINK_INFO", response.getInfoAboutLinkList());
+
+        }
+
+        return "linkAnalysis";
+    }
+}
