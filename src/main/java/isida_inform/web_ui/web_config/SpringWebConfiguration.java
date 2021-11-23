0543b63bd23e11113056cdcf9aeea4969dc7f9fa
AlexandrKozhekin <alexandrkozhekin@gmail.com>
2021-11-23 23:13:11 +0300

    add veb config

---
+++ src/main/java/isida_inform/web_ui/web_config/SpringWebConfiguration.java
@@ -0,0 +1,32 @@
+package isida_inform.web_ui.web_config;
+
+import org.springframework.context.annotation.*;
+
+import org.springframework.web.servlet.config.annotation.*;
+
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"isida_inform"})
+public class SpringWebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/");
+    }
+
+    @Bean
+    public SpringResourceTemplateResolver templateResolver() {
+        SpringResourceTemplateResolver resourceViewResolver = new SpringResourceTemplateResolver();
+        resourceViewResolver.setPrefix("classpath:/templates/");
+        resourceViewResolver.setSuffix(".html");
+        resourceViewResolver.setTemplateMode(TemplateMode.HTML);
+        resourceViewResolver.setCharacterEncoding("UTF-8");
+        resourceViewResolver.setCheckExistence(false);
+        return resourceViewResolver;
+    }
+
+}
