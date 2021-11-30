c6344cdf7160b704866d986aedfae72b9fe41d74
AlexandrKozhekin <alexandrkozhekin@gmail.com>
2021-11-30 21:01:10 +0300

    added additional validation

--- src/main/java/isida_inform/core/services/validators/LinkAnalysisValidator.java
+++ src/main/java/isida_inform/core/services/validators/LinkAnalysisValidator.java
@@ -1,8 +1,10 @@
 package isida_inform.core.services.validators;
 
-import isida_inform.core.requests.LinkAnalysisRequests;
+import isida_inform.core.databases.InfoAboutLinkImpl;
+import isida_inform.core.requests.LinkAnalysisRequest;
 import isida_inform.core.responses.CoreError;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.*;
@@ -10,25 +12,36 @@ import java.util.*;
 @Component
 public class LinkAnalysisValidator {
 
-    public Set<CoreError> validate(LinkAnalysisRequests request) {
+    @Autowired InfoAboutLinkImpl infoAboutLink;
+
+    public Set<CoreError> validate(LinkAnalysisRequest request) {
         Set<CoreError> errors = new HashSet<>();
+
         validateInputUrl(request).ifPresent(errors::add);
         validateUrl(request).ifPresent(errors::add);
+        zeroLinksReturn(request).ifPresent(errors::add);
+
         return errors;
     }
 
-    private Optional<CoreError> validateInputUrl(LinkAnalysisRequests request) {
+    private Optional<CoreError> validateInputUrl(LinkAnalysisRequest request) {
+
         return (request.getUrl() == null || request.getUrl().isEmpty())
-                ? Optional.of(new CoreError("", "не должен быть пустым!"))
+                ? Optional.of(new CoreError("url", "не должен быть пустым!"))
                 : Optional.empty();
     }
 
-    private Optional<CoreError> validateUrl(LinkAnalysisRequests requests) {
+    private Optional<CoreError> validateUrl(LinkAnalysisRequest requests) {
 
-        String url = requests.getUrl();
-
-        return (!url.startsWith("http") && !url.startsWith("https"))
+        return (!requests.getUrl().startsWith("http") && !requests.getUrl().startsWith("https"))
                 ? Optional.of(new CoreError("url", "ошибка ввода адреса анализируемой страницы!"))
+                : Optional.empty();
+    }
+
+    private Optional<CoreError> zeroLinksReturn(LinkAnalysisRequest requests) {
+
+        return (infoAboutLink.linkAnalysis(requests.getUrl()).isEmpty())
+                ? Optional.of(new CoreError("link", "анализируемая страница не содержет ссылок!"))
                 : Optional.empty();
     }
 
