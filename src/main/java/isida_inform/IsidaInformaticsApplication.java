9d179944ff110f4a8ab5b353693f7d6162866d63
AlexandrKozhekin <alexandrkozhekin@gmail.com>
2023-03-24 16:40:41 +0300

    moved to project root

---
+++ src/main/java/isida_inform/IsidaInformaticsApplication.java
@@ -0,0 +1,14 @@
+package isida_inform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsidaInformaticsApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(IsidaInformaticsApplication.class);
+
+    }
+}
